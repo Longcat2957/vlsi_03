e452297a951db70394595fdf71247a58f700fc9f
Longcat2957 <schrodingers030@gmail.com>
2022-07-17 15:30:04 +0900

    20220717 1530

--- utils/vis.py
+++ utils/vis.py
@@ -75,20 +75,5 @@ class CocoPoseVisualizer(Visualizer):
 
 
 if __name__ == '__main__':
-    limitations = [0, 1]
-    yoloobjects = YoloObjects(limitations)
-    dets_sample = [[100, 200, 300, 400, 0.9, 0], [100, 200, 300, 400, 0.9, 10]]
-    dets_sample = np.array(dets_sample)
-    yoloobjects.load_dets(dets_sample)
-    dets = yoloobjects(dets_sample)
-    print(dets.shape)
-
-    myyolovis = YoloVisualizer((480, 640), (576, 768))
-    test_image_path = '../input/test1.jpeg'
-    test_img = cv2.imread(test_image_path, cv2.IMREAD_COLOR)
-
-    test_img = myyolovis(test_img, dets)
-
-    cv2.imshow('image', test_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    
+    print('done')
