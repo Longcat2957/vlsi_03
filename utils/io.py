25f77a28a0b7e1c792bb717f881ab7ad2029fae6
Longcat2957 <schrodingers030@gmail.com>
2022-07-18 09:53:21 +0900

    20220718 1000

--- utils/io.py
+++ utils/io.py
@@ -1,5 +1,39 @@
 import cv2
 import os
+import subprocess
+import numpy as np
+
+class WriteVideo(object):
+    def __init__(self, name:str, width:int, height:int, fps:int):
+        self.name = name
+        self.width, self.height = width, height
+        self.fps = fps
+
+    def __call__(self):
+        return self._get_video_writer(self.name, self.width, self.height, self.fps)
+
+    def _get_video_writer(self, name, width, height, fps=30):
+        '''Get a VideoWriter object for saving output video.
+        This function tries to use Jetson's hardware H.264 encoder (omxh264enc)
+        if available, in which case the output video would be a MPEG-2 TS file.
+        Otherwise, it uses cv2's built-in encoding mechanism and saves a MP4
+        file.
+        '''
+        gst_elements = str(subprocess.check_output('gst-inspect-1.0'))
+        if 'omxh264dec' in gst_elements:
+            filename = name + '.ts'
+            gst_str = ('appsrc ! videoconvert ! omxh264enc ! mpegtsmux ! '
+                   'filesink location=%s') % filename
+            return cv2.VideoWriter(
+                gst_str, cv2.CAP_GSTREAMER, 0, fps, (width, height)
+            )
+        else:
+            filename = name + '.mp4'
+            return cv2.VideoWriter(
+                filename, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height)
+            )
+
+
 
 class LoadImage(object):
     
